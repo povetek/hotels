46a517937a27932e104d5225b1c3a6e11a184306
Dmitry Dutin <dmitrydutin@gmail.com>
2023-10-15 12:05:54 +0300

    feat: add taiga-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
+// Taiga-ui
+import { TuiRootModule, TuiDialogModule, TuiAlertModule } from '@taiga-ui/core';
+
+// Components
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
